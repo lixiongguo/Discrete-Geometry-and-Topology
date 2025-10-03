2e9bf193a620e9b6827a24acaeae6d9c318a0ede
李国雄 <lixiongguo>
2025-10-04 04:56:22 +0800

    增加lscm placeholder

--- projects/discrete-exterior-calculus/include/spectral-conformal-parameterization.h
+++ projects/discrete-exterior-calculus/include/spectral-conformal-parameterization.h
@@ -20,5 +20,6 @@ class SpectralConformalParameterization {
     VertexData<Vector2> flatten() const;
     VertexData<Vector2> tutte_flatten() const;
     std::vector<Vertex> getBoundaryVertices() const;
+    VertexData<Vector2> lscm_flatten() const;
     SparseMatrix<std::complex<double>> buildConformalEnergy() const;
 };
